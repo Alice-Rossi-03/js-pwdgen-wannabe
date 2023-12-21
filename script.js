5877ba85796184391f99b9883f77ccae6d54243a
Alice-Rossi-03 <alicerossi0310@gmail.com>
2023-12-21 15:59:34 +0100

    feat (javascript) /variabili

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+let nomeUtente = prompt("Inserisci qui il tuo nome"); 
+let cognomeUtente = prompt("Inserisci qui il tuo cognome"); 
+let coloreUtente = prompt("Inserisci qui il tuo colore preferito"); 
+
+document.writeln('nomeUtente' + '/' + 'cognomeUtente' + '/' + 'coloreUtente'); 
+ 
